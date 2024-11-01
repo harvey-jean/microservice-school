f272e3ec66916781e8e4818dfb1dffaf69eff9b5
harvey-jean <ihevea@gmail.com>
2024-11-01 12:35:37 +0100

    Initial Commit

---
+++ school/src/main/java/com/harvey/school/SchoolController.java
@@ -0,0 +1,31 @@
+package com.harvey.school;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/schools")
+@RequiredArgsConstructor
+public class SchoolController {
+    private final SchoolService schoolService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void save(@RequestBody School school) {
+        schoolService.saveSchool(school);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<School>> findAllStudents(){
+        return ResponseEntity.ok(schoolService.findAllSchools());
+    }
+
+    @GetMapping("/with-students/{school-id}")
+    public ResponseEntity<FullSchoolResponse> findSchoolWithStudents(@PathVariable("school-id") Integer schoolId){
+        return ResponseEntity.ok(schoolService.findSchoolsWithStudents(schoolId));
+    }
+}
---
+++ student/src/main/java/com/harvey/student/Student.java
@@ -0,0 +1,24 @@
+package com.harvey.student;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Student {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Integer schoolId;
+}
